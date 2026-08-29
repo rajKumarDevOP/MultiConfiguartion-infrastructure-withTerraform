645d943aa637a09cb5397d7d99bc2ab87cf4cbd0
Raj Kumar <144013786+rajKumarDevOP@users.noreply.github.com>
2026-08-29 12:00:21 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,34 +1,34 @@
 locals {
   servers = {
     frontend = {
-      ami           = "ami-01a00762f46d584a1"
+      ami           = "ami-01xxxxxxxxx"
       instance_type = "t3.medium"
       root_volume   = 15
     #   eip           = "eipalloc-..."
       name          = "Frontend-server"
     }
 
-    # backend = {
-    #   ami           = "ami-..."
-    #   instance_type = "t3a.medium"
-    #   root_volume   = 75
-    # #   eip           = "eipalloc-..."
-    #   name          = "backend-server"
-    # }
-    # kafka = {
-    #   ami           = "ami-..."
-    #   instance_type = "t3a.medium"
-    #   root_volume   = 50
-    # #   eip           = "eipalloc-..."
-    #   name          = "kafka-server"
-    # }
+     backend = {
+      ami           = "ami-02xxxxxxxxx"
+       instance_type = "t3a.medium"
+       root_volume   = 75
+     #   eip           = "eipalloc-..."
+       name          = "backend-server"
+     }
+     kafka = {
+       ami           = "ami-03xxxxxxxxx"
+       instance_type = "t3a.medium"
+       root_volume   = 50
+     #   eip           = "eipalloc-..."
+       name          = "kafka-server"
+     }
 
-    # auth = {
-    #   ami           = "ami-..."
-    #   instance_type = "t3a.medium"
-    #   root_volume   = 50
-    # #   eip           = "eipalloc-..."
-    #   name          = "auth-server"
-    # }
+     auth = {
+       ami           = "ami-04xxxxxxxxx"
+       instance_type = "t3a.medium"
+       root_volume   = 50
+     #   eip           = "eipalloc-..."
+       name          = "auth-server"
+     }
   }
 }
